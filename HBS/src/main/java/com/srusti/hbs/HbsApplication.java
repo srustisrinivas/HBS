d7e08bf2e3795abe3412ca2ed8027be4b25ceaf9
srustisrinivas <46515268+srustisrinivas@users.noreply.github.com>
2019-01-09 12:05:45 +0530

    Add files via upload

---
+++ HBS/src/main/java/com/srusti/hbs/HbsApplication.java
@@ -0,0 +1,12 @@
+package com.srusti.hbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HbsApplication.class, args);
+	}
+}
